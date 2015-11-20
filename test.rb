0ba567910772ea7a8a74f60e5b6116acddd82945
Carliff Carleel <carliffrizal.carleel@gmail.com>
2015-11-20 17:08:56 +0800

    completed

---
+++ test.rb
@@ -0,0 +1,19 @@
+require_relative 'app'
+	
+# cohort = Cohort.find(1)
+# cohort[:name] = 'Best cohort Ever!'
+# cohort.save
+
+# student = Student.find(1)
+# student[:first_name] = 'Carliff Rizal'
+# student.save
+
+# Student.find(1)[:first_name] = 'Carliff Rizal'
+# p Student.all
+
+p Cohort.find(1)#[:name] = 'Best cohort Ever!'
+# p Cohort.all
+
+# Cohort.create(:name => "Carliff")
+# p Cohort.all
+
